a78dfdbb1d5db2d123cfa3e9244230bb69f9b2a4
ayushparmar857@gmail.com <ayushparmar857@gmail.com>
2025-12-07 18:28:15 +0530

    - add Project files

---
+++ Payment page/payment.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Payment Form</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1>Confirm Your Payment</h1>
+        <div class="first-row">
+            <div class="owner">
+                <h3>Enter UPI-ID</h3>
+                <div class="input-field">
+                    <input type="text">
+                </div>
+            </div>
+                    <input type="submit" value="Pay" class="pay">
+                
+        </div>
+    
+                <div class="cards">
+                    <img src="mc.png" alt="">
+                    <img src="vi.png" alt="">
+                    <img src="pp.png" alt="">
+                </div>
+
+                <div class="qr">
+                    <img src="QR2.jpg" alt="" height="220px" width="210px">
+                </div>
+   
+        <!-- <a href="">Confirm</a> -->
+    </div>
+</body>
+</html>
